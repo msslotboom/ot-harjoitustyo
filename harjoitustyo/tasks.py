87d6f198170ff2cb27cd76d7f9523705c061ef49
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-04-05 20:31:30 +0300

    Add invokes

---
+++ harjoitustyo/tasks.py
@@ -0,0 +1,12 @@
+from invoke import task
+@task
+def start(ctx):
+    ctx.run("python3 src/main.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("coverage run --branch -m pytest src",pty=True)
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src",pty=True)
