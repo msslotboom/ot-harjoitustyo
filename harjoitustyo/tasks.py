20fdb94be35c5e9e7bcd197cb21f443b28646ccd
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-04-10 13:18:31 +0300

    Added invoke format

--- harjoitustyo/tasks.py
+++ harjoitustyo/tasks.py
@@ -10,3 +10,7 @@ def test(ctx):
 @task
 def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src",pty=True)
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
