cf94867cbeb5092c5ead7d5ed206b51f5befce2f
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2022-04-16 10:38:13 +0300

    Refractored gameloop

---
+++ harjoitustyo/src/gameloop.py
@@ -0,0 +1,42 @@
+from cgitb import handler
+import pygame, level
+
+class Gameloop():
+    def __init__(self, bg_color, size) -> None:
+        self.clock = pygame.time.Clock()
+        self.tempsize = (size)
+        self.screen = pygame.display.set_mode(self.tempsize)
+        self.gamelevel = level.Level(self.screen)
+        self.bg_color = bg_color
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                   self.gamelevel.robot_move_left()
+                if event.key == pygame.K_RIGHT:
+                   self.gamelevel.robot_move_right()
+                if event.key == pygame.K_UP:
+                   self.gamelevel.robot_jump()
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                   self.gamelevel.robot_move_right()
+                if event.key == pygame.K_RIGHT:
+                   self.gamelevel.robot_move_left()
+
+    def render(self):
+        self.screen.fill(self.bg_color)
+        self.gamelevel.all_sprites.draw(self.screen)
+        self.gamelevel.refresh()
+        pygame.display.flip()
+        self.clock.tick(60)
+
+    def start(self):
+        while True:
+            if self.handle_events() == False:
+                break
+            self.render()
--- harjoitustyo/src/main.py
+++ harjoitustyo/src/main.py
@@ -1,48 +1,13 @@
 import pygame
-import level
+import gameloop
 
 
 def main():
     pygame.init()
-    #info = pygame.display.Info()
-    #size = (info.current_w, info.current_h)
-    # temporary size, will need to be updated to be fullscreen
-    tempsize = (600, 480)
-    screen = pygame.display.set_mode(tempsize)
-    clock = pygame.time.Clock()
-    gamelevel = level.Level(screen)
-
-    running = True
-
-    # Gameloop to be refractored into other file
-    while running:
-        screen.fill((255, 87, 87))
-        gamelevel.all_sprites.draw(screen)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    gamelevel.robot_move_left()
-                if event.key == pygame.K_RIGHT:
-                    gamelevel.robot_move_right()
-                if event.key == pygame.K_UP:
-                    gamelevel.robot_jump()
-
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    gamelevel.robot_move_right()
-                if event.key == pygame.K_RIGHT:
-                    gamelevel.robot_move_left()
-        #print(gamelevel.robot.rect.x, gamelevel.robot.rect.y)
-        gamelevel.refresh()
-        pygame.display.flip()
-        clock.tick(60)
-
-    pygame.quit()
-
-
+    bg_color = (255, 87, 87)
+    size = (600, 480)
+    loop = gameloop.Gameloop(bg_color, size)
+    loop.start()
+    
 if __name__ == "__main__":
     main()
